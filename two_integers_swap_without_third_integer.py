89e1939fe359336dce4b86078f6ba982870b9cc9
Furkan <f.mentes1905@gmail.com>
2022-02-11 01:22:20 +0300

    Python code
    two integers swap without third integer using python

---
+++ two_integers_swap_without_third_integer.py
@@ -0,0 +1,10 @@
+x= int(input("Please enter first number a= " ))
+y= int(input("Please enter the second number b= "))
+
+x= x-y
+y=x+y
+x=y-x
+
+print(f"a = {x}",f"b = {y}")
+
+#print("a=",x,"b=",y)
